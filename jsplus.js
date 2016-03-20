b30b9a1d9e1ab386eadbd5eef1fdb81ad89ace79
Tiến Suitie <tiensuitie@gmail.com>
2016-03-21 01:44:27 +0700

    Update jsplus.js

--- jsplus.js
+++ jsplus.js
@@ -1,3 +1,7 @@
 String.prototype.chr = function(num){
+  if(num === undefined) num = 0;
   return this[num < 0 ? this.length + num : num];
+};
+String.prototype.reverse = function(){
+  return this.split("").reverse().join("");
 };
