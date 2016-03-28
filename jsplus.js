909cf3d02f45def39301f7ed7b108d9071403389
Tiến Suitie <tiensuitie@gmail.com>
2016-03-28 12:18:00 +0700

    Update jsplus.js

--- jsplus.js
+++ jsplus.js
@@ -1,7 +1,39 @@
+const or = function(){
+  var a = arguments, i;
+  for(i=1; i<a.length; i++)
+    if(a[0] === a[i]) return true;
+  return false;
+};
+const and = function(){
+  var a = arguments, i;
+  for(i=1; i<a.length; i++)
+    if(a[0] !== a[i]) return false;
+  return true;
+};
+const type = function(o){
+  if( or(o + "", "null", "NaN", "Infinity", "-Infinity") )
+    return (o + "").toLowerCase();
+  else if( or(typeof o, "string", "number", "boolean", "function", "undefined") )
+    return typeof o;
+  return (o.constructor + "").slice(9, -20).replace(/^HTML|Element$/, "").toLowerCase() || "element";
+};
+Math.rand = function(num1, num2){
+  var a, b;
+  if(num2 === undefined) num2 = 0;
+  a = Math.min(num1, num2);
+  b = Math.max(num1, num2);
+  return Math.floor(a + Math.random() * (b + 1 - a));
+};
+Math.hyp = function(x1, y1, x2, y2){
+  switch( arguments.length ){
+    case 2: return Math.sqrt( Math.pow( Math.abs(x1), 2 ) + Math.pow( Math.abs(y1), 2 ) ); break;
+    case 4: return Math.sqrt( Math.pow( Math.abs(x2 - x1), 2 ) + Math.pow( Math.abs(y2 - y1), 2 ) ); break;
+  }
+};
 Number.prototype.fixed = function(num){
   return +this.toFixed(num);
 };
-Number.prototype.addZeroes(len){
+Number.prototype.addZeroes = function(len){
   var a, b;
   return (this < 0 ? "-" : "") + "0".repeat(len - (a = (b = (Math.abs(this) + "").split("."))[0]).length) + a + (b.length < 2 ? "" : "." + b[1]);
 };
@@ -12,19 +44,38 @@ String.prototype.chr = function(num){
 String.prototype.reverse = function(){
   return this.split("").reverse().join("");
 };
-String.prototype.trimMultiSpaces = function(){
-  return this.replace(/\s+\s+/g, " ");
-};
 Array.prototype.clone = function(){
   return this.concat();
 };
-Date.prototype.daysInMonth = function(){
-  var t = new Date(this), d = 28;
-  while(new Date(t.setDate(d)).getDate() > 1){
-    d++;
-  }
-  return d - 1;
+Array.prototype.unique = function(){
+  var i;
+  for(i=0; i<this.length; i++)
+    while(i != this.lastIndexOf( this[i] )) this.splice(this.lastIndexOf( this[i] ), 1);
 };
-Date.prototype.daysInYear = function(){
-  return new Date("2 29 " + this.getFullYear()).getDate() > 1 ? 366 : 365;
+Array.prototype.isErratic = function(){
+  var i;
+  for(i=1; i<this.length; i++)
+    if( this[0] !== this[i] && JSON.stringify(this[0]) !== JSON.stringify(this[i]) ) return true;
+  return false;
+};
+Array.prototype.isSame = function(ary){
+  var i;
+  if( this.length !== ary.length ) return false;
+  if( JSON.stringify( this ) !== JSON.stringify( ary ) ) return false;
+  for(i=0; i<this.length; i++)
+    if( type( this[i] ) !== type( ary[i] ) || typeof this[i] !== typeof ary[i] ) return false;
+  return true;
+}
+Array.prototype.shuffle = function(same){
+  var a, b, r;
+  do {
+    a = [];
+    b = this.concat();
+    while(b.length > 0){
+      a.push( b[r = Math.rand( b.length - 1 )] );
+      b.splice(r, 1);
+    }
+  }
+  while(this.isSame(a) && !same && a.isErratic());
+  return a;
 };
